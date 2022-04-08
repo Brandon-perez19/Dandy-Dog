8d8b6c8809f6ed5c90cfbba4f021143d332416d3
Brandon <brandonperez@brandons-mbp.lan>
2022-04-07 23:03:14 -0400

    added functionality that adds breed heading on top of dog picture

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -16,30 +16,74 @@ fetch(apiUrl).then(function(response) {
 var displayPicture = function(message){
     for (let i = 0; i < 3; i++) {
         console.log(message[i]);
+        console.log(message[i].split("/")[4]);
+
         //selects empty div to hold img-divs
         var dogResults = document.querySelector(".daily-dog-result");
-        //creates div holding elemtent
+
+        //creates <div> holding element
         var imgHolder = document.createElement("div");
-        //set bulma style class for divs
-        imgHolder.setAttribute("class", "column " + "is-one-third")
-        //set style
+
+        //set bulma style class for imgHolder
+        imgHolder.setAttribute("class", "column " + "is-one-third");
+
+        //set style for imgHolder
         imgHolder.style.textAlign = "center";
-        //creates img html
+
+        //creates <a> element
+        var imgLink = document.createElement("a");
+
+        //set href value for imgLink
+        imgLink.setAttribute("href", message[i]);
+        imgLink.setAttribute("target", "_blank");
+
+        //creates <img> element
         var imgCreate = document.createElement("img");
-        //set style of img
-        // imgCreate.setAttribute("class", "image" + " is-128x128")
-        //sets href value
+
+        //sets imgCreate href value
         imgCreate.setAttribute("src", message[i]);
-        //set style
+
+        //set imgCreate styles
         imgCreate.style.borderRadius = "20%";
         imgCreate.style.height = "200px";
         imgCreate.style.width = "200px";
         imgCreate.style.display = "inline";
-        imgCreate.style.border = "solid 6px #F7EDE2"
-        //append to holder div
-        imgHolder.appendChild(imgCreate);
+        imgCreate.style.border = "solid 6px #F7EDE2";
+
+        //create heading for dog breed image
+        var dogHeading = document.createElement("h2");
+
+        //creating dogHeading text content
+        var breedText = message[i].split("/")[4];
+        var breedArray= breedText.split("-");
+
+        // set dogHeading text content
+        if (breedArray[1]){
+            var breedTitle1 = breedArray[1];
+            var breedTitle2 = breedArray[0];
+            var breedTitle1Uppercase = breedTitle1.charAt(0).toUpperCase() + breedTitle1.slice(1);
+            var breedTitle2Uppercase = breedTitle2.charAt(0).toUpperCase() + breedTitle2.slice(1);
+            var breedName = breedTitle1Uppercase + " " + breedTitle2Uppercase;
+            dogHeading.textContent = breedName;
+        } else {
+            var breedTitle = breedArray[0];
+            var breedName = breedTitle.charAt(0).toUpperCase()+ breedTitle.slice(1);
+            dogHeading.textContent = breedName
+        };
+        //create dogHeading styles
+
+        //append to imgHolder
+        imgHolder.appendChild(dogHeading);
+
+        //append to imgLink
+        imgLink.appendChild(imgCreate);
+
+        //append to imgHolder
+        imgHolder.appendChild(imgLink);
+
         //append to empty div that is hard coded
         dogResults.appendChild(imgHolder);
 
     };
 };
+
