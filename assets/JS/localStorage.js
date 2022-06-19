f3659169129a27f0cf5ddd73ce8f7fdcf9a8bc95
Brandon <brandonperez@brandons-mbp.lan>
2022-06-19 16:17:55 -0400

    fixed local ztorage error of storig array of objects everytime. Fixed populate storage function error duplicating search results. removed search field.

--- assets/JS/localStorage.js
+++ assets/JS/localStorage.js
@@ -1,16 +1,24 @@
 //saves search results to local storage
-var saveToLocalStorage = function (breedObj) {
-    var breeds = [];
+var breeds = [];
+
+var grabFromLocalStorage = function () {
     if (localStorage.getItem('breeds')) {
-        console.log('local storage detected')
-        breeds.push(JSON.parse(localStorage.getItem('breeds')));
-        breeds.push(breedObj)
-    } else {
-        console.log('no local storage detected')
-        breeds.push(breedObj);
+        var parseLocalStorage = JSON.parse(localStorage.getItem('breeds'))
+        for (let i = 0; i < parseLocalStorage.length; i++) {
+            var breedObj ={
+                breed:parseLocalStorage[i].breed,
+                url: parseLocalStorage[i].url
+            };
+
+            breeds.push(breedObj);
+        }
     }
+}
+
+var saveToLocalStorage = function (breedObj) {
+    breeds.push(breedObj);
     localStorage.setItem("breeds", JSON.stringify(breeds));
-    populateLocalStorage(breeds)
+    populateLocalStorage(breedObj);
 }
 
 var populateLocalStorage = function (breeds) {
@@ -18,17 +26,20 @@ var populateLocalStorage = function (breeds) {
     console.log(results)
     var recentSearchesResults = document.querySelector('.local-storage-results');
     if (results) {
+        recentSearchesResults.innerHTML = ' '
         for (let i = 0; i < results.length; i++) {
             var breedName = results[i].breed
             var breedUrl = results[i].url
 
-            console.log(breedName, breedUrl)
+            console.log(breedName, breedUrl);
 
             var recentSearchesTitle = document.createElement('a');
             var recentSearchesList = document.createElement('ul');
 
             recentSearchesTitle.textContent = breedName
             recentSearchesTitle.setAttribute('href', breedUrl);
+            recentSearchesTitle.setAttribute("target", "_blank");
+
             recentSearchesList.appendChild(recentSearchesTitle);
             recentSearchesResults.appendChild(recentSearchesList);
         }
@@ -37,4 +48,5 @@ var populateLocalStorage = function (breeds) {
     }
 };
 
+grabFromLocalStorage();
 populateLocalStorage();
